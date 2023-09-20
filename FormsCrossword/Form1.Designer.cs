b78f2f1bf8c8b313f27a0abe100ea014f4c64db4
User <User@DESKTOP-RBMPGAB>
2023-09-20 10:11:19 +0700

    Добавьте файлы проекта.

---
+++ FormsCrossword/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace FormsCrossword
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(170, 170);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(170, 170, 0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(230, 230);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.CausesValidation = false;
+            this.flowLayoutPanel2.Controls.Add(this.flowLayoutPanel1);
+            this.flowLayoutPanel2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(400, 400);
+            this.flowLayoutPanel2.TabIndex = 1;
+            this.flowLayoutPanel2.WrapContents = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+    }
+}
+
---
+++ FormsCrossword/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FormsCrossword
+{
+    public partial class Form1 : Form
+    {
+        int x;
+        string array
+        int[,] array =
+        {
+            { 1, 0, 1, 0, 1, 1, 0, 1, 0, 0},
+            { 0, 0, 0, 0, 1, 1, 0, 0, 1, 0},
+            { 1, 0, 1, 0, 0, 1, 0, 0, 1, 0},
+            { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 1, 1, 0, 0, 1, 0, 1, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+            { 1, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+            { 1, 0, 1, 0, 1, 0, 0, 0, 0, 0},
+        };
+        
+        public void checkBoxArray()
+        {
+            int k = array.Length;
+            for (int i = 0; i < k; i++)
+            {
+                CheckBox checkBx = new CheckBox();
+                checkBx.Size = new Size(15,15);
+                checkBx.Click += check_click;
+                flowLayoutPanel1.Controls.Add(checkBx);
+            }
+        }
+        public void check_click(object sender, EventArgs e)
+        {
+            CheckBox checkBx = (CheckBox)sender;
+        }
+        public void chekI()
+        {
+            double a = Math.Sqrt(array.Length);
+            int ArrWidth = Convert.ToInt32(a);
+            for (int i = 0; i < a-1; i++)
+            {
+                for (int j = 0; j < a-1; j++)
+                {
+                    if (array[i,j] == 1)
+                    {
+                        x++;
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            //тут чек на кол-во цифер и установка размера
+            checkBoxArray();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        
+    }
+}
